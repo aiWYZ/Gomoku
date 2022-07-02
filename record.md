4f8a3b9d568af0525c3a609a4a973b178effd272
Kizunawl <2194732270@qq.com>
2022-07-02 17:15:53 +0800

    wjd updated his fresh dull submission

--- record.md
+++ record.md
@@ -1,3 +1,7 @@
 ylt created test.py (useless) to test.
 
 2022-07-02 10:22 wjd commited his submission(dull and very dull)
+
+**2022-07-02 17:12 wjd commited his updated dull submission**
+**A step costs about 3 sec when 10 or more chesses on board**
+**你们看看有什么能继续加速的，以及棋形有没有更好的分类/判断方法**
--- submission_wjd.py
+++ submission_wjd.py
@@ -1,145 +1,177 @@
 import numpy as np
 
-max_layer = 3
+board_width = 0
+board_height = 0
+state_map = []
+chess_player_idx = 0
+opponent_idx = 0
+max_layer = 2
+ans = [7, 7]
+chess_type = [[], ['11111', '011110', '1111', '11101', '11011', '10111', '01110', '111', '1011', '1101', '0110', '11', '101'],
+    ['22222', '022220', '2222', '22202', '22022', '20222', '02220', '222', '2022', '2202', '0220', '22', '202']]
 
-def get_available_attempt(state_map, board_width, board_height):
-    state_map_ex = np.zeros((board_width+4, board_height+4))
-    state_map_ex[2:board_width+2, 2:board_height+2] = state_map.copy()
-
-    chess_around = (state_map_ex[0:-4, 0:-4]                            + state_map_ex[0:-4, 2:-2]                            + state_map_ex[0:-4, 4:]+
-                    state_map_ex[1:-3, 0:-4] + state_map_ex[1:-3, 1:-3] + state_map_ex[1:-3, 2:-2] + state_map_ex[1:-3, 3:-1] + state_map_ex[1:-3, 4:]+
-                    state_map_ex[2:-2, 0:-4] + state_map_ex[2:-2, 1:-3]                            + state_map_ex[2:-2, 3:-1] + state_map_ex[2:-2, 4:]+
-                    state_map_ex[3:-1, 0:-4] + state_map_ex[3:-1, 1:-3] + state_map_ex[3:-1, 2:-2] + state_map_ex[3:-1, 3:-1] + state_map_ex[3:-1, 4:]+
-                    state_map_ex[4:, 0:-4]                              + state_map_ex[4:, 2:-2]                              + state_map_ex[4:, 4:])
+def get_available_attempt():
+    state_map_expand = np.zeros((board_width+2, board_height+2))
+    state_map_expand[1:board_width+1, 1:board_height+1] = state_map.copy()
+    chess_around = (state_map_expand[0:-2, 0:-2] + state_map_expand[0:-2, 1:-1] + state_map_expand[0:-2, 2:]+
+                    state_map_expand[1:-1, 0:-2]                                + state_map_expand[1:-1, 2:]+
+                    state_map_expand[2:  , 0:-2] + state_map_expand[2:  , 1:-1] + state_map_expand[2:,   2:])
 
     available_state_map = (chess_around>0) & (state_map==0)
     available_attempt = np.where(available_state_map==True)
-
     if len(available_attempt[0])==0:
-        return np.array([[board_width//2], [board_height//2]])
+        return [[7,7]]
 
-    return available_attempt
-    # where chess is available to put
-    # there is chess(whatever it belongs) within distance of 2
+    att = []
+    for i in range(len(available_attempt[0])):
+        att.append([available_attempt[0][i], available_attempt[1][i]])
+    return att
+    """
+    周围一格内有棋子才去考虑
+    返回一个n*2的list，表示可能可以落子的坐标
+    """
 
 
-def get_score(state_map, attempt_x, attempt_y, player_idx):
+def get_score(s, player_idx):
+    if (chess_type[player_idx][0] in s):
+        return 1e7
+    elif (chess_type[player_idx][1] in s):
+        return 10000
+    elif ((chess_type[player_idx][2] in s) or (chess_type[player_idx][3] in s) or (chess_type[player_idx][4] in s) 
+        or (chess_type[player_idx][5] in s)):
+        return 1000
+    elif (chess_type[player_idx][6] in s):
+        return 500
+    elif ((chess_type[player_idx][7] in s) or (chess_type[player_idx][8] in s) or (chess_type[player_idx][9] in s)):
+        return 200
+    elif (chess_type[player_idx][10] in s):
+        return 100
+    elif (chess_type[player_idx][11] in s) or (chess_type[player_idx][12] in s):
+        return 20
+    else:
+        return 1
+    """
+    简单粗暴，帮我想想其他棋形可能长什么样
+    """
 
-    def get_diag_line(state_map, attempt_x, attempt_y):
-        tmp=[]
-        stx, sty, enx, eny= 0, 0, 0, 0
-        if (attempt_x<5 or attempt_y<5):
-            stx, sty = attempt_x-min(attempt_x, attempt_y), attempt_y-min(attempt_x, attempt_y)
-        else:
-            stx, sty = attempt_x-5, attempt_y-5
-        if (attempt_x>9 or attempt_y>9):
-            enx, eny = attempt_x+min(15-attempt_x, 15-attempt_y), attempt_y+min(15-attempt_x, 15-attempt_y)
-        else:
-            enx, eny = attempt_x+6, attempt_y+6
-        for i,j in zip(range(stx, enx), range(sty, eny)):
-            tmp.append(state_map[i][j])
-        return str(tmp)
 
-    line = []
-    line.append(str(state_map[attempt_x, max(0, attempt_y-5):min(15, attempt_y+6)]).replace(',','').replace(' ',''))
-    line.append(str(state_map[max(0, attempt_x-5):min(15, attempt_x+6), attempt_y]).replace(',','').replace(' ',''))
-    line.append(get_diag_line(state_map, attempt_x, attempt_y))
-    line.append(get_diag_line(np.fliplr(state_map), attempt_x, 14-attempt_y))
-    # line[row, column, diag, diag-rev] of nextby 9 blank
+def evaluate():
+    def evaluate_(idx):
+        score = 0
+        for i in range(board_width):
+            line = str(state_map[i,:]).replace(',','').replace(' ','')
+            score += get_score(line, idx)
+            line = str(state_map[:, i]).replace(',','').replace(' ','')
+            score += get_score(line, idx)
+
+        state_map_fliplr = np.fliplr(state_map)
+        for i in range(-14, 15):
+            line = str(np.diagonal(state_map, offset=i)).replace(',','').replace(' ','')
+            score += get_score(line, idx)
+            line = str(np.diagonal(state_map_fliplr, offset=i)).replace(',','').replace(' ','')
+            score += get_score(line, idx)
+        return score
 
     score = 0
-    chess_type = [[], ['11111', '011110', '1111', '11101', '11011', '10111', '01110', '111', '1011', '1101', '0110', '11', '101'],
-                    ['22222', '022220', '2222', '22202', '22022', '20222', '02220', '222', '2022', '2202', '0220', '22', '202']]
-
-    for i in range(4):
-        if (chess_type[player_idx][0] in line[i]):
-            return (score, True)
-        elif (chess_type[player_idx][1] in line[i]):
-            score += 100000
-        elif ((chess_type[player_idx][2] in line[i]) or (chess_type[player_idx][3] in line[i]) or (chess_type[player_idx][4] in line[i]) 
-            or (chess_type[player_idx][5] in line[i])):
-            score += 10000
-        elif (chess_type[player_idx][6] in line[i]):
-            score += 8000
-        elif ((chess_type[player_idx][7] in line[i]) or (chess_type[player_idx][8] in line[i]) or (chess_type[player_idx][9] in line[i])):
-            score += 1000
-        elif (chess_type[player_idx][10] in line[i]):
-            score += 100
-        elif (chess_type[player_idx][11] in line[i]) or (chess_type[player_idx][12] in line[i]):
-            score += 20
-        else:
-            score += 5
-
-    return (score, False)
+    score += evaluate_(chess_player_idx)
+    score -= evaluate_(opponent_idx)*0.1
+    return score
+    """
+    对整个棋盘打分
+    横/竖/主对角线/副对角线
+    +自己得分-对手得分
+    """
 
 
-def max_decision(state_map, board_width, board_height, chess_player_idx, depth):
-    # print('max', depth)
+def is_winning():
+    def check(win_check):
+        for i in range(board_width):
+            if ((win_check in str(state_map[i,:]).replace(',','').replace(' ','')) or
+                (win_check in str(state_map[:, i]).replace(',','').replace(' ',''))):
+                return True
 
-    available_attempt = get_available_attempt(state_map, board_width, board_height)
-    score = np.zeros(len(available_attempt[0]))
-    
-    idx = 0
-    for i in range(len(available_attempt[0])):
-        state_map[available_attempt[0][i]][available_attempt[1][i]] = chess_player_idx
-        score[i], winning = get_score(state_map.copy(), available_attempt[0][i], available_attempt[1][i], chess_player_idx)
-        score[i] *= (0.8**depth)
-        if (winning==True):
-            if (depth>0):
-                return (score[i], True)
-            else:
-                return (available_attempt[0][i], available_attempt[1][i])
-        if (depth<=max_layer):
-            score_next_layer, losing = min_decision(state_map, board_width, board_height, 3-chess_player_idx, depth)
-            if (losing==True):
-                state_map[available_attempt[0][i]][available_attempt[1][i]] = 0
-                continue
-            score[i] += score_next_layer
-        state_map[available_attempt[0][i]][available_attempt[1][i]] = 0
-        if (score[i]>score[idx]):
-            idx = i
+        state_map_fliplr = np.fliplr(state_map)
+        for i in range(-10, 11):
+            if ((win_check in str(np.diagonal(state_map, offset=i)).replace(',','').replace(' ',''))
+                or (win_check in str(np.diagonal(state_map_fliplr, offset=i)).replace(',','').replace(' ',''))):
+                return True
 
-    if (depth>0):
-        return (score[idx], False)
+    if (check('11111')):
+        return True
+    if (check('22222')):
+        return True
+    return False
+
+
+def maxmin_decision(depth, is_opponent, alpha, beta):
+    if (depth==max_layer) or is_winning():
+        return evaluate()
+
+    available_attempt = get_available_attempt()
+    global ans
+
+    if (is_opponent):
+        score = 1e9
+        for step in available_attempt:
+            state_map[step[0]][step[1]] = opponent_idx
+            score = min(score, maxmin_decision(depth+1, False, alpha, beta))
+            state_map[step[0]][step[1]] = 0
+            if (score<beta):
+                beta = score
+                if (alpha>=beta):
+                    return alpha
+        return beta
+        # 对手落子 挑尽可能小的 score取min
+        # 返回的值是我们在上一层向这一层挑（呃呃
+        # 返回的值尽可能大
+        # 如果对手在这一支路挑出来的分数已经不大于我们在其他支路的得分 beta<=alpha（这里的支路指从上一层扩散出去的）
+        # 就直接结束 因为如果我们挑了这条路 对手一定选当前这个beta让我们得到的分数变小
+
     else:
-        return (available_attempt[0][idx], available_attempt[1][idx])
+        score = -1e9
+        target = []
+        for step in available_attempt:
+            state_map[step[0]][step[1]] = chess_player_idx
+            tmpscore = maxmin_decision(depth+1, True, alpha, beta)
+            state_map[step[0]][step[1]] = 0
+            if (tmpscore>score):
+                score = tmpscore
+                target = step
+            if (score>alpha):
+                alpha = score
+                if (alpha>=beta):
+                    if (depth==0):
+                        ans = step
+                    return beta
+        if (depth==0):
+            ans = target
+        return alpha
+        # 我们落子 挑尽可能大的 score取max
+        # 对手挑我们 返回值希望尽可能小
+        # 如果我们在当前支路返回的分数已经不小于对手在其他支路的得分 对手一定不会挑当前这条支路
+
+    """
+    1.如果到最大迭代次数或者出现有人赢了 直接开始算分数
+    2.获取可能可以落子的坐标
+    3.对每个坐标枚举
+    """
     
-
-def min_decision(state_map, board_width, board_height, chess_player_idx, depth):
-    # print('min', depth)
-
-    available_attempt = get_available_attempt(state_map, board_width, board_height)
-    score = np.zeros(len(available_attempt[0]))
-    
-    idx = 0
-    for i in range(len(available_attempt[0])):
-        state_map[available_attempt[0][i]][available_attempt[1][i]] = chess_player_idx
-        score[i], losing = get_score(state_map.copy(), available_attempt[0][i], available_attempt[1][i], chess_player_idx)
-        score[i] *= (0.8**depth)*(-0.6)
-        if (losing==True):
-            return (0, True)
-        if (depth<max_layer):
-            score_next_layer, winning = max_decision(state_map, board_width, board_height, 3-chess_player_idx, depth+1)
-            if (winning==True):
-                state_map[available_attempt[0][i]][available_attempt[1][i]] = 0
-                continue
-            score[i] += score_next_layer
-        state_map[available_attempt[0][i]][available_attempt[1][i]] = 0
-        if (score[i]>score[idx]):
-            idx = i
-
-    return (score[idx], False)
-
 
 def my_controller(observation, action_space, is_act_continuous=False):
+    global board_width
+    global board_height
+    global chess_player_idx
+    global opponent_idx
+    global state_map
     board_width = observation['board_width']
     board_height = observation['board_height']
     chess_player_idx = observation['chess_player_idx']
     state_map = np.array(observation['state_map'])
     state_map = state_map[:,:,0]
+    opponent_idx = 3-chess_player_idx
 
+    final_score = maxmin_decision(depth=0, is_opponent=False, alpha=-1e9, beta=1e9)
     output = [[0]*15, [0]*15]
-    answer_x, answer_y = max_decision(state_map, board_width, board_height, chess_player_idx, depth=0)
-    output[0][answer_x] = 1
-    output[1][answer_y] = 1
+    output[0][ans[0]] = 1
+    output[1][ans[1]] = 1
     return output
